6bc211cde58a35eb8ee7d3f30ad9e395653e63a0
ciriast <ciriastorres@gmail.com>
2025-04-14 06:20:26 -0600

    Add increases functions

--- main.c
+++ main.c
@@ -12,6 +12,14 @@ int sumTwoNumbers() {
     return num1 + num2;
 }
 
+void increase_x(struct position* snake_position) {
+    snake_position->x += 1;
+}
+
+void increase_y(struct position* snake_position) {
+    snake_position->y += 1;
+}
+
 void draw_board(int rows, int cols) {
     struct position snakePosition;
     snakePosition.x = 2;
@@ -54,7 +62,15 @@ void draw_board(int rows, int cols) {
 int main() {
     printf("Welcome to Snake made in C \n");
 
-    draw_board(10, 10);
+    struct position snake_position = {.x = 5, .y = 5};
+    printf("snake initial position in x: %d\n", snake_position.x);
+    printf("snake initial position in y: %d\n", snake_position.y);
+
+    increase_x(&snake_position);
+    increase_y(&snake_position);
+
+    printf("new snake position in x: %d\n", snake_position.x);
+    printf("new snake position in y: %d\n", snake_position.y);
 
     return 0;
 }
